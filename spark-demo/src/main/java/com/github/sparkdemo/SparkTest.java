c2da3a33965e98e80280229d906dbaf1ca4b3e1f
jinrunheng <1175088275@qq.com>
2021-11-25 11:16:15 +0800

    update demo

--- spark-demo/src/main/java/com/github/sparkdemo/SparkTest.java
+++ spark-demo/src/main/java/com/github/sparkdemo/SparkTest.java
@@ -5,6 +5,7 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
@@ -13,36 +14,40 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class SparkTest {
+    private static void sparkTest() {
 
-    public static void main(String[] args) {
+        String inputFilePath = "/Users/macbook/Desktop/myProject/spark-learing/spark-demo/src/main/resources/hello.txt";
+        String outputFilePath = "/Users/macbook/Desktop/myProject/spark-learing/spark-demo/src/main/resources/output.txt";
+
         SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        String filePath = "/Users/macbook/Desktop/myProject/spark-learing/spark-demo/src/main/resources/test.txt";
-        String outputFilePath = "/Users/macbook/Desktop/myProject/spark-learing/spark-demo/src/main/resources/outputFile.txt";
-        JavaRDD<String> input = sc.textFile(filePath).cache();
-        // 切分为单词
-        JavaRDD<String> words = input.flatMap(
-                new FlatMapFunction<String, String>() {
+        JavaRDD<String> input = sc.textFile(inputFilePath);
+        JavaRDD<String> words = input.flatMap((FlatMapFunction<String, String>) s ->  Arrays.asList(s.split(" ")).iterator());
+        System.out.println();
+        JavaPairRDD<String, Integer> counts = words.mapToPair(
+                (PairFunction<String, String, Integer>) s -> new Tuple2<>(s, 1)
+        ).reduceByKey((Function2<Integer, Integer, Integer>) (x, y) -> x + y);
+        counts.saveAsTextFile(outputFilePath);
+    }
 
-                    public Iterable<String> call(String s) throws Exception {
-                        return Arrays.asList(s.split(" "));
+    private static void testRDDFilter(){
+        String logPath = "/Users/macbook/Desktop/myProject/spark-learing/spark-demo/src/main/resources/log.txt";
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> inputRDD = sc.textFile(logPath);
+        JavaRDD<String> errorRDD = inputRDD.filter(
+                new Function<String, Boolean>() {
+                    @Override
+                    public Boolean call(String s) throws Exception {
+                        return s.contains("error");
                     }
                 }
         );
+        System.out.println(errorRDD.count());
+    }
 
-        JavaPairRDD<String, Integer> counts = words.mapToPair(
-                new PairFunction<String, String, Integer>() {
-                    @Override
-                    public Tuple2<String, Integer> call(String s) throws Exception {
-                        return new Tuple2<>(s, 1);
-                    }
-                }
-        ).reduceByKey(new Function2<Integer, Integer, Integer>() {
-            @Override
-            public Integer call(Integer x, Integer y) throws Exception {
-                return x + y;
-            }
-        });
-        counts.saveAsTextFile(outputFilePath);
+    public static void main(String[] args) {
+        sparkTest();
+        // testRDDFilter();
     }
 }
