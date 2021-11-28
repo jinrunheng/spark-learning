22e9224510f3b59ebfe91f3a293b1adc0d75cc13
jinrunheng <1175088275@qq.com>
2021-11-28 23:13:43 +0800

    第九章未完成

--- spark-demo/src/main/java/com/github/sparkdemo/chapter9/SparkSqlTest.java
+++ spark-demo/src/main/java/com/github/sparkdemo/chapter9/SparkSqlTest.java
@@ -1,5 +1,6 @@
 package com.github.sparkdemo.chapter9;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -9,9 +10,23 @@ import org.apache.spark.sql.hive.HiveContext;
 public class SparkSqlTest {
 
     public static void sparkSqlTest() {
-        JavaSparkContext ctx = new JavaSparkContext();
+
+        // 建立与 Spark 的连接
+        SparkConf conf = new SparkConf()
+                .setAppName("Spark SQL")
+                .setMaster("local");
+
+        JavaSparkContext ctx = new JavaSparkContext(conf);
         SQLContext hiveContext = new HiveContext(ctx);
         String inputFilePath = "/Users/macbook/Desktop/myProject/spark-learing/spark-demo/src/main/resources/chapter9/testweet.json";
-        SchemaRDD input  = hiveContext.jsonFile(inputFilePath);
+        Dataset<Row> rowDataset = hiveContext.jsonFile(inputFilePath);
+        // 注册输入的 SchemaRDD
+        rowDataset.registerTempTable("tweets");
+        // 依据 retweetCount（转发计数）选出推文
+        Dataset<Row> topTweets = hiveContext.sql("SELECT text,retweetCount FROM tweets ORDER BY retweetCount LIMIT 10");
+    }
+
+    public static void main(String[] args) {
+        sparkSqlTest();
     }
 }
